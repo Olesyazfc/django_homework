59a4bddc902d33ebaee6cad341846344abd0e728
Olesyazfc <lesyi2001@mail.ru>
2022-06-15 00:15:56 +0600

    Append recipes and padination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+import csv
+from pagination.settings import BUS_STATION_CSV
+from django.core.paginator import Paginator
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
+
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
+    with open(BUS_STATION_CSV, encoding='UTF-8', newline='') as f:
+        read = csv.DictReader(f)
+        stations_list = []
+        for row in read:
+            stations_list.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(stations_list, 20)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
+
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -10,21 +10,25 @@ DATA = {
         'макароны, г': 0.3,
         'сыр, г': 0.05,
     },
-    'buter': {
+    'butter': {
         'хлеб, ломтик': 1,
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipe_view(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA.get(dish, None)
+    result = {}
+    for item, value in recipe.items():
+        new_value = value * servings
+        result[item] = round(new_value, 2)
+
+    context = {
+        'recipe': result
+    }
+    return render(request, 'calculator/index.html', context)
+
+
